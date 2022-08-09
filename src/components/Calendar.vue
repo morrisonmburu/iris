1e822b7d61ef5679b38211eed297ff95d947d4eb
morrisonmburu <morrisonmburu7@gmail.com>
2022-08-09 11:52:25 +0300

    initial commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div>Calendar Goes here</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Calendar',
+    data: function () {
+        return {}
+    }
+}
+</script>
